793dc1fb552fee912e87e9707455682537571176
Kenajcrap <Kenajcrap@protonmail.com>
2023-03-05 19:14:06 -0300

    preliminary tests

---
+++ tests/longtest.sh
@@ -0,0 +1,6 @@
+demoknight -g 440 -o ./raw -n 20 -k -f csv -v DEBUG -s 49000 -d 20 -D habib_i63_grandfinals_2018-08-26_22-31-53
+demoknight -g 440 -o ./raw -n 20 -k -f csv -v DEBUG -s 49000 -d 20 -D habib_i63_grandfinals_2018-08-26_22-31-53
+demoknight -g 440 -o ./raw -n 20 -k -f csv -v DEBUG -s 49000 -d 20 -D habib_i63_grandfinals_2018-08-26_22-31-53
+demoknight -g 440 -o ./raw -n 20 -k -f csv -v DEBUG -s 49000 -d 20 -D habib_i63_grandfinals_2018-08-26_22-31-53
+demoknight -g 440 -o ./raw -n 20 -k -f csv -v DEBUG -s 49000 -d 20 -D habib_i63_grandfinals_2018-08-26_22-31-53
+demoknight -g 440 -o ./raw -n 20 -k -f csv -v DEBUG -s 49000 -d 20 -D habib_i63_grandfinals_2018-08-26_22-31-53
